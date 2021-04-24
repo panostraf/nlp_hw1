552fcdce2d449269008a6f2cfac4af70ee888418
panostraf <p.trafalis@gmail.com>
2021-04-24 03:44:31 +0300

    add stopwords function and simplify token_functions

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import tools
+import settings
 
-def main(file_name):
-    counter = tools.Counter(file_name)
-    counter.paragraph_counter()
-    # counter.results()
+
+# TODO: 
+# Word Cloud
+# Readme File
+# Report
+# move results function to main.py
+# Add comments to the code
+
+
+
+
+
+def main():
+    fname = settings.filenames['fname']
+    counter = tools.Counter(fname)
+    counter.paragraph_tokenize()
+    counter.sentences_tokenize()
+    counter.word_tokenize()
+    counter.results()
+    counter.drop_stop_words()
+
 
 
 if __name__ == '__main__':
-
-    fname = 'sample.txt'
-    main(file_name=fname)
+    main()
--- tools.py
+++ tools.py
@@ -5,29 +5,19 @@ from nltk.tokenize import sent_tokenize
 class Counter:
     def __init__(self, file_name):
         self.words = {}
-        self.paragraphs = 0
-        self.sentences = 0
-        self.unique_words = 0
         self.file_name = file_name
         self.str_file = open(self.file_name, 'r').read()
 
-
-    def paragraph_counter(self):
+    def paragraph_tokenize(self):
         pars = [par for par in self.str_file.split('\n') if len(par) > 0]
-        for pr in pars:
-            self.paragraphs += 1
-            self.sentences_counter(pr)
+        return len(pars)
 
+    def sentences_tokenize(self):
+        setences = sent_tokenize(self.str_file)
+        return len(setences)
 
-    def sentences_counter(self, par):
-        setences = sent_tokenize(par)
-        for sen in setences:
-            self.word_counter(sen)
-            self.sentences += 1
-
-
-    def word_counter(self, sen):
-        words = [self.word_replacements(word) for word in sen.split()]
+    def word_tokenize(self):
+        words = [self.word_replacements(word) for word in self.str_file.split()]
         words = [word.split() for word in words]
         for word in words:
             if isinstance(word, list):
@@ -36,9 +26,7 @@ class Counter:
             else:
                 self.punc_replacements(word)
 
-
     def word_replacements(self, item):
-
         if item.isalnum()==False:
             for key in settings.rule_overide:
                 item = item.replace(key, settings.rule_overide[key])
@@ -49,11 +37,9 @@ class Counter:
         else:
             return item
             
-
     def punc_replacements(self, word):
 
         while word[0] in settings.puncuations:
-
             if len(word)>1:
                 self.add_to_dict(word[0])
                 word = word[1:]
@@ -62,16 +48,14 @@ class Counter:
                 break
 
         while word[-1:] in settings.puncuations:
-
             if len(word)>1:            
                 self.add_to_dict(word[-1:])
                 word = word[:-1]
             else:
                 self.add_to_dict(word)
-                break
-                    
-        self.add_to_dict(word)
+                break   
 
+        self.add_to_dict(word)
 
     def add_to_dict(self, word):
         if word in self.words.keys():
@@ -79,21 +63,24 @@ class Counter:
         else:
             self.words[word] = 1
 
-
     def results(self):
         with open('results.txt','w') as f:
+            
+            text = f'''
+Here are the results of the text:
+Number of paragraphs:  {self.paragraph_tokenize()}\n
+Number of sentences:  {self.sentences_tokenize()}\n
+Number of words:  {sum(self.words.values())}\n
+Number of unique words:  {len(self.words.keys())}\n
+----------------------------------------
+Here is the word frequency:
+'''
+            word_frequency = '\n'
 
-            t0 = '''\nHere are the results of the text:\n'''
-            t1=f'The number of paragraphs in text is:  {self.paragraphs}\n'
-            t2=f'The number of sentences in text is:  {self.sentences}\n'
-            t3=f'The number of words in text is:  {sum(self.words.values())}\n'
-            t4=f'The number of unique words in text is:  {len(self.words.keys())}\n'
-            t5 = '----------------------------------------'
-            t6 = '\nHere is the word frequency:\n'
-            f.writelines([t0,t1,t2,t3,t4,t5,t6])
             for key,value in self.sort_dict().items():
-                    f.writelines(f'{key}:{value}\n')
+                word_frequency = word_frequency + f'{key} - {value}\n'
 
+            f.writelines([text,word_frequency])
 
     def sort_dict(self):
         dict_ = {}
@@ -101,12 +88,20 @@ class Counter:
             my_item = (w, self.words[w])
             dict_[w] = self.words[w]
         return dict_
-        # sorted_dict = sorted(self.words, key=self.words.get, reverse=True)
-        # print(sorted_dict[10])
+
+    def drop_stop_words(self):
+        stop_words = ['that','the']
+        print(len(self.words))
+        for word in stop_words:
+            self.words.pop(word)
+        print(len(self.words))
 
 
 if __name__ == '__main__':
     fname = settings.filenames['fname']
     counter = Counter(fname)
-    counter.paragraph_counter()
+    counter.paragraph_tokenize()
+    counter.sentences_tokenize()
+    counter.word_tokenize()
     counter.results()
+    counter.drop_stop_words()
